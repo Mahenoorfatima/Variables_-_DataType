3532eaf6e1eb9c185f6bc5cc324a25eedb8f65fe
Mahenoor Fatima <136818372+Mahenoorfatima@users.noreply.github.com>
2024-06-12 21:43:56 +0500

    Add files via upload

---
+++ Variables & Data Type/app.js
@@ -0,0 +1,25 @@
+const product = {
+    name: "Ball Pen",
+    rating: 4,
+    offer: "5% off",
+    price: 500, 
+    isDeal: true
+};
+
+console.log(product);
+
+
+// Const Object
+
+const profile = {
+    userName: "Mahenoor Fatima" ,
+    post: 12 ,
+    followers : 500,
+    following : 4,
+    isFollow: true,
+    informations: "Privte Account",
+};
+
+console.log(profile);
+console.log(profile.userName);
+console.log(profile["followers"]);
